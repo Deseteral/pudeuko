b724c592908d0b2fde2d5183b01d919f66d9c035
Deseteral <deseteral@gmail.com>
2019-01-30 12:47:38 +0100

    Load port from env

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+use rocket::{self, get, routes, Config};
+use std::env;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+fn configure() -> Config {
+    let mut config = Config::active().expect("could not load configuration");
+    if let Ok(port_str) = env::var("PORT") {
+        let port = port_str.parse().expect("could not parse PORT");
+        config.set_port(port);
+    }
+    config
+}
+
 fn main() {
-    println!("main log");
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::custom(configure()).mount("/", routes![index]).launch();
 }
