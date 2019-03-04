15a8c2a4c129fa80146ed89088dcf01f865e7590
Deseteral <deseteral@gmail.com>
2019-03-04 20:16:24 +0100

    Preparing the rocket

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,13 @@ mod items_controller;
 mod pudeuko_service;
 mod dropbox_client;
 
-use rocket::{self, routes, Config};
+use rocket::{self, Rocket, routes};
+use pudeuko_service::PudeukoService;
 
-fn create_configuration(port: &u16) -> Config {
-    let mut config = Config::active().expect("Could not load configuration");
-    config.set_port(*port);
-    config
-}
+fn rocket(port: u16, pudeuko_service: PudeukoService) -> Rocket {
+    let mut config = rocket::Config::active().expect("Could not load configuration");
+    config.set_port(port);
 
-fn main() {
-    let config = config::Config::load_from_env();
-    let dropbox_client = dropbox_client::DropboxClient::new(&config.dropbox_token);
-    let pudeuko_service = pudeuko_service::PudeukoService::new(dropbox_client);
-
-    let config = create_configuration(&config.port);
     rocket::custom(config)
         .manage(pudeuko_service)
         .mount("/items", routes![
@@ -27,5 +20,12 @@ fn main() {
             items_controller::post_item,
             items_controller::get_item,
         ])
-        .launch();
+}
+
+fn main() {
+    let config = config::Config::load_from_env();
+    let dropbox_client = dropbox_client::DropboxClient::new(&config.dropbox_token);
+    let pudeuko_service = pudeuko_service::PudeukoService::new(dropbox_client);
+
+    rocket(config.port, pudeuko_service).launch();
 }
