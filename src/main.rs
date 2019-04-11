7297ef384f0c75d2efed6142965dd2acb6096b3d
Deseteral <deseteral@gmail.com>
2019-04-11 20:15:39 +0200

    Simplify config import

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ mod infrastructure;
 mod pudeuko_service;
 
 use actix_web::{web, App, HttpServer};
+use config::Config;
 use infrastructure::DropboxStorage;
 use pudeuko_service::{PudeukoService, SharedPudeukoService};
 
 fn main() -> std::io::Result<()> {
-    let app_config = config::Config::load();
-    let dropbox_storage = DropboxStorage::new(&app_config.dropbox_token);
+    let config = Config::load();
+    let dropbox_storage = DropboxStorage::new(&config.dropbox_token);
     let pudeuko_service = PudeukoService::new(Box::new(dropbox_storage));
     let shared_service = PudeukoService::make_shared(pudeuko_service);
 
@@ -22,6 +23,6 @@ fn main() -> std::io::Result<()> {
             .route("/items", web::post().to(api::items_endpoint::post_item))
             .route("/items/{id}", web::get().to(api::items_endpoint::get_item))
     })
-    .bind(format!("127.0.0.1:{}", &app_config.port))?
+    .bind(format!("127.0.0.1:{}", &config.port))?
     .run()
 }
