e4da1f2c5395b6e4064472aad15cd59ed51488a9
Deseteral <deseteral@gmail.com>
2019-04-11 19:54:43 +0200

    Send + Sync for Storage

--- src/infrastructure/storage.rs
+++ src/infrastructure/storage.rs
@@ -1,7 +1,7 @@
 use crate::domain::ItemList;
 
 #[allow(clippy::ptr_arg)]
-pub trait Storage {
+pub trait Storage: Send + Sync {
     fn read(&self) -> ItemList;
     fn write(&mut self, list: ItemList);
 }
