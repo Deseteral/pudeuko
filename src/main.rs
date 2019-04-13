25a70785dcd664906c535f14e3324d314a2b2a6f
Deseteral <deseteral@gmail.com>
2019-04-13 04:18:51 +0200

    Removed test log

--- src/main.rs
+++ src/main.rs
@@ -35,16 +35,14 @@ fn main() -> std::io::Result<()> {
     setup_logging().expect("Failed to initializer logger");
 
     let config = Config::load();
-
-    info!("test");
-    info!("Storage type: {}", config.storage_type);
-
     let storage: Box<dyn Storage> = match config.storage_type {
         StorageType::Dropbox => Box::new(DropboxStorage::new(&config.dropbox_token)),
         StorageType::InMemory => Box::new(InMemoryStorage::new()),
     };
     let service = PudeukoService::new(storage);
     let shared_service = PudeukoService::make_shared(service);
+
+    info!("Storage type: {}", config.storage_type);
 
     HttpServer::new(move || {
         App::new()
