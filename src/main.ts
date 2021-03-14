e8078816364704f90f2527cea2e1291c9b6a9bfd
Deseteral <deseteral@gmail.com>
2021-03-14 10:40:19 +0100

    Basic server stuff

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000, () => {
+  console.log('pudeuko started on port 3000');
+});
