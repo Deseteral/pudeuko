c248604189fe7921b66ba12cf581217c613d0c3c
Deseteral <deseteral@gmail.com>
2021-03-15 23:19:58 +0100

    Delete pudeuko item

--- src/pudeuko-controller.ts
+++ src/pudeuko-controller.ts
@@ -1,6 +1,11 @@
 import Router from '@koa/router';
 import { Context } from 'koa';
-import { PudeukoService } from './pudeuko-service';
+import { PudeukoItemNotFoundError, PudeukoService } from './pudeuko-service';
+
+const STATUS_OK = 200;
+const STATUS_NO_CONTENT = 204;
+const STATUS_NOT_FOUND = 404;
+const STATUS_INTERNAL_SERVER_ERROR = 500;
 
 interface ContentDTO {
   text: string,
@@ -15,6 +20,7 @@ class PudeukoController {
     this.router.get('/', PudeukoController.getPudeuko);
     this.router.post('/', PudeukoController.addItem);
     this.router.get('/:id', PudeukoController.getItem);
+    this.router.delete('/:id', PudeukoController.removeItem);
   }
 
   private static async getPudeuko(ctx: Context): Promise<void> {
@@ -22,24 +28,22 @@ class PudeukoController {
       const pudeuko = await PudeukoService.getPudeuko();
 
       ctx.body = pudeuko;
-      ctx.status = 200;
+      ctx.status = STATUS_OK;
     } catch (e) {
       console.error(e);
-      ctx.status = 500;
+      ctx.status = STATUS_INTERNAL_SERVER_ERROR;
     }
   }
 
   private static async addItem(ctx: Context): Promise<void> {
     const content: ContentDTO = ctx.request.body;
 
-    ctx.body = '';
-
     try {
       await PudeukoService.addItemFromText(content.text);
-      ctx.status = 200;
+      ctx.status = STATUS_OK;
     } catch (e) {
       console.error(e);
-      ctx.status = 500;
+      ctx.status = STATUS_INTERNAL_SERVER_ERROR;
     }
   }
 
@@ -49,15 +53,33 @@ class PudeukoController {
     try {
       const item = await PudeukoService.getItem(itemId);
 
-      if (item) {
-        ctx.body = item;
-        ctx.status = 200;
-      } else {
-        ctx.status = 404;
-      }
+      ctx.body = item;
+      ctx.status = STATUS_OK;
     } catch (e) {
       console.error(e);
-      ctx.status = 500;
+
+      if (e instanceof PudeukoItemNotFoundError) {
+        ctx.status = STATUS_NOT_FOUND;
+      } else {
+        ctx.status = STATUS_INTERNAL_SERVER_ERROR;
+      }
+    }
+  }
+
+  private static async removeItem(ctx: Context): Promise<void> {
+    const itemId = ctx.params.id;
+
+    try {
+      await PudeukoService.removeItem(itemId);
+      ctx.status = STATUS_NO_CONTENT;
+    } catch (e) {
+      console.error(e);
+
+      if (e instanceof PudeukoItemNotFoundError) {
+        ctx.status = STATUS_NOT_FOUND;
+      } else {
+        ctx.status = STATUS_INTERNAL_SERVER_ERROR;
+      }
     }
   }
 
@@ -67,49 +89,3 @@ class PudeukoController {
 }
 
 export default PudeukoController;
-
-// pub fn get_items(shared_service: web::Data<SharedPudeukoService>) -> HttpResponse {
-//     let service = shared_service.lock().unwrap();
-//     let list = service.get_all();
-
-//     HttpResponse::Ok().json(list)
-// }
-
-// pub fn post_item(
-//     content: web::Json<ContentDTO>,
-//     shared_service: web::Data<SharedPudeukoService>,
-// ) -> HttpResponse {
-//     let mut service = shared_service.lock().unwrap();
-//     let item = Item::from(content.0);
-
-//     service.add_item(item.clone());
-//     HttpResponse::Ok().json(item)
-// }
-
-// pub fn get_item(
-//     path: web::Path<(String,)>,
-//     shared_service: web::Data<SharedPudeukoService>,
-// ) -> HttpResponse {
-//     let service = shared_service.lock().unwrap();
-//     let id = &path.0;
-
-//     match service.get_item_by_id(&id) {
-//         Some(item) => HttpResponse::Ok().json(item),
-//         None => HttpResponse::NotFound().body(format!("Item with id '{}' was not found", &id)),
-//     }
-// }
-
-// pub fn delete_item(
-//     path: web::Path<(String,)>,
-//     shared_service: web::Data<SharedPudeukoService>,
-// ) -> HttpResponse {
-//     let mut service = shared_service.lock().unwrap();
-//     let id = &path.0;
-
-//     if service.get_item_by_id(&id).is_some() {
-//         service.remove_item_by_id(&id);
-//         HttpResponse::NoContent().body(format!("Item with id '{}' was removed", &id))
-//     } else {
-//         HttpResponse::NotFound().body(format!("Item with id '{}' was not found", &id))
-//     }
-// }
--- src/pudeuko-service.ts
+++ src/pudeuko-service.ts
@@ -2,14 +2,25 @@ import { nanoid } from 'nanoid';
 import DropboxStorage from './dropbox-storage';
 import { PudeukoItem, PudeukoObject } from './model';
 
+class PudeukoItemNotFoundError extends Error {
+  constructor(itemId: string) {
+    super(`Pudeuko does not contain item with id: ${itemId}`);
+    this.name = 'PudeukoItemNotFoundError';
+  }
+}
+
 class PudeukoService {
   static async getPudeuko(): Promise<PudeukoObject> {
     return DropboxStorage.read();
   }
 
-  static async getItem(itemId: string): Promise<PudeukoItem | undefined> {
+  static async getItem(itemId: string): Promise<PudeukoItem> {
     const pudeuko = await DropboxStorage.read();
-    return pudeuko.items.find((item) => item.id === itemId);
+    const item = pudeuko.items.find((it) => it.id === itemId);
+
+    if (!item) throw new PudeukoItemNotFoundError(itemId);
+
+    return item;
   }
 
   static async addItemFromText(text: string): Promise<void> {
@@ -20,7 +31,17 @@ class PudeukoService {
     await DropboxStorage.write(pudeuko);
   }
 
-  static mapTextToItem(text: string): PudeukoItem {
+  static async removeItem(itemId: string): Promise<void> {
+    const pudeuko = await DropboxStorage.read();
+    const index = pudeuko.items.findIndex((item) => item.id === itemId);
+
+    if (index === -1) throw new PudeukoItemNotFoundError(itemId);
+
+    pudeuko.items.splice(index, 1);
+    await DropboxStorage.write(pudeuko);
+  }
+
+  private static mapTextToItem(text: string): PudeukoItem {
     return {
       id: nanoid(),
       createdAt: new Date(),
@@ -31,4 +52,5 @@ class PudeukoService {
 
 export {
   PudeukoService,
+  PudeukoItemNotFoundError,
 };
